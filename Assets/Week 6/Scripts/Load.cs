f40f0768dc247ab99b87cff85528e4d7a1532295
casiana-atki <atkicasi@shernet.sheridancollege.ca>
2024-02-15 12:52:43 -0500

    Week6-Task2 Completed! From Scene A to B, press the button. From SceneB to Week5, press ENTER and I added an extra "Escape" feature on Week5 to return to the main screen.

---
+++ Assets/Week 6/Scripts/Load.cs
@@ -0,0 +1,20 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+
+    }
+}
