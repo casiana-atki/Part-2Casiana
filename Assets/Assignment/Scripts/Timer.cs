e160c4ccaf78c9924af21ee1731f943549dbcdd1
casiana-atki <atkicasi@sheridancollege.ca>
2024-02-21 12:31:58 -0500

    WIP for Timert text

--- Assets/Assignment/Scripts/Timer.cs
+++ Assets/Assignment/Scripts/Timer.cs
@@ -5,23 +5,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Slider = UnityEngine.UI.Slider;
-
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
     public float maxTime = 60;
     public Slider slider;
+/*    TextMeshProUGUI textMeshPro;
+    string timer;
+    int timeLeft;
+    public GameObject timertext;*/
    
     void Start()
     {
-
+       // textMeshPro = timertext.GetComponent<TextMeshProUGUI>();
     }
 
     public void Update()
     {
         maxTime -= 1 * Time.deltaTime;
         slider.value = maxTime;
-
+      /*  timeLeft = (int)maxTime;
+        timer = timeLeft.ToString();
+        textMeshPro.text = timeLeft.ToString();*/
+        
 
     }
 
